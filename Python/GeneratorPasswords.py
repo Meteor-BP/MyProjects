ebec00647a86d359cee037d936297910e2942dd5
Meteor-BP <bor4547@gmail.com>
2024-08-29 15:11:51 +0300

    Обновление 2.0
    Просто везде поставил русский

--- Python/GeneratorPasswords.py
+++ Python/GeneratorPasswords.py
@@ -1,12 +1,12 @@
 import random
 
-print("Generator by MeteorBP")
+print("Генератор сделан MeteorBP // Версия 2.0")
 while True:
     chars = list('-_wabcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
-    length = int(input('Length Password?'+ "\n"))
+    length = int(input('Длина пароля:'+ "\n"))
     random.shuffle(chars)
     pasw = ''.join([random.choice(chars) for x in range(length)])
     print('')
-    print(f'Your Password: {pasw}')
-    print('Press Enter to exit')
+    print(f'Ваш паоль: {pasw}')
+    print('Нажмите enter чтобы выйти...')
     print('')
